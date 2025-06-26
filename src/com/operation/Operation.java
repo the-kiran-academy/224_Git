a818f85efaef131d1a61d6fdd9eed5ceae76746c
sayali-dhanvij <sayalidhanvij54@gmail.com>
2025-06-27 03:06:13 +0530

    evenno

--- src/com/operation/Operation.java
+++ src/com/operation/Operation.java
@@ -1,18 +1,26 @@
 package com.operation;
 
 public class Operation {
-	
+
 	public static void isEven(int number) {
-		// sayali dhanvij 
-		
+		if (number % 2 == 0) {
+			System.out.println(number + " is even.");
+		} else {
+			System.out.println(number + " is odd.");
+		}
+	}
+
+	public static void main(String[] args) {
+		isEven(4); // Example call
+		isEven(7);
 	}
 
 	public static void isOdd(int number) {
-		 // Swati Belokar
+		// Swati Belokar
 	}
 
 	public static void isPrime(int number) {
-		// Ashvini Ingle 
+		// Ashvini Ingle
 	}
 
 	public static void isPerfect(int number) {
@@ -20,27 +28,27 @@ public class Operation {
 	}
 
 	public static void isArmstrong(int number) {
-		// akshada Fartale 
+		// akshada Fartale
 	}
 
 	public static void isPalindrome(int number) {
 		// kailas jare
 	}
-	
+
 	public static void isFibonacci(int number) {
 		// Dip Kale
 	}
-	
+
 	public static void isStrong(int number) {
 		// Dhanashree Kadawale
 	}
-	
+
 	public static void isHarshad(int number) {
-		// Bhad Vaishnavi Gorkhnath 
+		// Bhad Vaishnavi Gorkhnath
 	}
-	
+
 	public static void reverseString(String name) {
-		// Sandip Ugile 
+		// Sandip Ugile
 	}
 
 	public static void isAnagram(String name1, String name2) {
@@ -54,37 +62,37 @@ public class Operation {
 	public static void isVowel(char ch) {
 		// sameer turkar
 	}
-	
+
 	public static void isConsonant(char ch) {
 		// Shruti Gaikwad
 	}
-	
+
 	public static void isLeapYear(int year) {
 		// Rushikesh Kharode
 	}
-	
+
 	public static void sumOfArray(int[] arr) {
-		// Abhishek Kokitkar 
+		// Abhishek Kokitkar
 	}
-	
+
 	public static void isSorted(int[] arr) {
 		// Ganesh Chougule
 	}
-	
+
 	public static void decendingArray(int[] arr) {
-		// Tejas Bagad 
+		// Tejas Bagad
 	}
-	
+
 	public static void ascendingArray(int[] arr) {
 		// Likhita Bhangale
 	}
-	
+
 	public static void smallestElement(int[] arr) {
 
 	}
-	
+
 	public static void largestElement(int[] arr) {
 
 	}
-	
+
 }
