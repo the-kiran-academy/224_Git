e27aff691be80e51994243255a6998f26d5ce850
sameerturkar <sameerturkar1@gmail.com>
2025-06-27 16:38:37 +0530

    isvowel is added

--- src/com/operation/Operation.java
+++ src/com/operation/Operation.java
@@ -1,70 +1,70 @@
 package com.operation;
 
 public class Operation {
-	
+
 	public static void isEven(int number) {
-		// sayali dhanvij 
-		
+		// sayali dhanvij
+
 	}
 
 	public static void isOdd(int number) {
-		 // Swati Belokar
+		// Swati Belokar
 	}
 
 	public static void isPrime(int number) {
-		// Ashvini Ingle 
+		// Ashvini Ingle
 	}
 
 	public static void isPerfect(int number) {
 		// Yash Mandle
 	}
 
-	 public static void isArmstrong(int number) {
-	        int originalNumber = number;
-	        int result = 0;
-	        int n = 0;
+	public static void isArmstrong(int number) {
+		int originalNumber = number;
+		int result = 0;
+		int n = 0;
 
-	        // Count number of digits
-	        int temp = number;
-	        while (temp != 0) {
-	            temp /= 10;
-	            n++;
-	        }
+		// Count number of digits
+		int temp = number;
+		while (temp != 0) {
+			temp /= 10;
+			n++;
+		}
 
-	        // Calculate sum of nth powers of digits
-	        temp = number;
-	        while (temp != 0) {
-	            int digit = temp % 10;
-	            result += Math.pow(digit, n);
-	            temp /= 10;
-	        }
+		// Calculate sum of nth powers of digits
+		temp = number;
+		while (temp != 0) {
+			int digit = temp % 10;
+			result += Math.pow(digit, n);
+			temp /= 10;
+		}
 
-	        // Check if it is an Armstrong number
-	        if (result == originalNumber) {
-	            System.out.println(originalNumber + " is an Armstrong number.");
-	        } else {
-	            System.out.println(originalNumber + " is not an Armstrong number.");
-	        }
-	    }
+		// Check if it is an Armstrong number
+		if (result == originalNumber) {
+			System.out.println(originalNumber + " is an Armstrong number.");
+		} else {
+			System.out.println(originalNumber + " is not an Armstrong number.");
+		}
+	}
 
 	public static void isPalindrome(int number) {
 		// kailas jare
 	}
-	
+
 	public static void isFibonacci(int number) {
 		// Dip Kale
 	}
-	
+
 	public static void isStrong(int number) {
 		// Dhanashree Kadawale
 	}
-	
+
 	public static void isHarshad(int number) {
-		// Bhad Vaishnavi Gorkhnath 
+		// Bhad Vaishnavi Gorkhnath
 	}
-	
+
 	public static void reverseString(String name) {
-		// Sandip Ugile 
+		// Sandip Ugile
 	}
 
 	public static void isAnagram(String name1, String name2) {
@@ -76,39 +76,48 @@ public class Operation {
 	}
 
 	public static void isVowel(char ch) {
+
 		// sameer turkar
+
+		ch = Character.toLowerCase(ch);
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println("Charater is Vowel: " + ch);
+
+		} else {
+			System.out.println("Charater is Not Vowel.");
+		}
 	}
-	
+
 	public static void isConsonant(char ch) {
 		// Shruti Gaikwad
 	}
-	
+
 	public static void isLeapYear(int year) {
 		// Rushikesh Kharode
 	}
-	
+
 	public static void sumOfArray(int[] arr) {
-		// Abhishek Kokitkar 
+		// Abhishek Kokitkar
 	}
-	
+
 	public static void isSorted(int[] arr) {
 		// Ganesh Chougule
 	}
-	
+
 	public static void decendingArray(int[] arr) {
-		// Tejas Bagad 
+		// Tejas Bagad
 	}
-	
+
 	public static void ascendingArray(int[] arr) {
 		// Likhita Bhangale
 	}
-	
+
 	public static void smallestElement(int[] arr) {
 
 	}
-	
+
 	public static void largestElement(int[] arr) {
 
 	}
-	
+
 }
